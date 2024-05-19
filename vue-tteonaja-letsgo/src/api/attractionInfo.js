4125ba072dbb075a5a8c4a7128d464dc275c9dad
swLeesw <wlk256032@gmail.com>
2024-05-20 01:08:47 +0900

    feat: 관광지 검색 기능 추가, 관광지 api 모듈화

---
+++ vue-tteonaja-letsgo/src/api/attractionInfo.js
@@ -0,0 +1,30 @@
+import { localAxios } from "@/util/http-commons";
+
+const local = localAxios();
+
+function getAttraction(params, success, fail) {
+    local
+        .get(`/attractionInfo/region`, { params: params})
+        .then(success)
+        .catch(fail);
+}
+
+function getSido(success, fail) {
+    local
+        .get(`/attractionInfo/sido`)
+        .then(success)
+        .catch(fail);
+}
+
+function getGugun(param, success, fail) {
+    local
+        .get(`/attractionInfo/gugun/${param}`)
+        .then(success)
+        .catch(fail)
+}
+
+export {
+    getAttraction,
+    getSido,
+    getGugun
+  };
